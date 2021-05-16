710e1b9b75c913f4cafba7001eb1e718069128f1
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2021-05-16 16:13:17 +0900

    いろいろ試してみる

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -1,9 +1,24 @@
 defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
-  def index(conn, _params) do
+  def index(conn, %{"_format" => "text"}) do
+    conn
+    |> put_status(202)
+    |> text("""
+      return
+      text
+      """)
+  end
+
+  def index(conn, %{"l" => "test"}) do
     conn
     |> put_layout("test.html")
+    |> render("index.html")
+  end
+
+
+  def index(conn, _params) do
+    conn
     |> render("index.html")
   end
 end
